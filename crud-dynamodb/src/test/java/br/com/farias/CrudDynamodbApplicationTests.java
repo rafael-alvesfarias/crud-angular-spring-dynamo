88813c6ccb3829bab6eb67a87a921783d3b45e59
rafael-alvesfarias <rafael.alvesfarias@outlook.com>
2021-01-12 11:30:28 -0300

    inicializacao do projeto de backend com spring initializer

---
+++ crud-dynamodb/src/test/java/br/com/farias/CrudDynamodbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.farias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDynamodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
